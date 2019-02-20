6d8b3ecb67a1535c0d9861b30e9fbb02275a7529
Saer Debel <saer.debel@gmail.com>
2019-02-20 11:11:19 +1100

    finished python exercises

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # this will be the main file
 # author: Saer Debel
 
+import os
+
 x = 2
 print(x)
 
@@ -9,6 +11,29 @@ names = ["saer", 3]
 
 for i in names:
     print(i)
+
+if x == 2:
+    x += 1
+
+print(x)
+
+#num_words = int(input("Enter a number: "))
+
+def print_words(num_words):
+    list_variable = []
+
+    for _ in range(num_words):
+        list_variable.append(input(""))
+
+    space = " "
+    print(space.join(list_variable), end=' ')
+
+
+print_words(3)
+
+print(names[0:2])
+
+
 
 
 
